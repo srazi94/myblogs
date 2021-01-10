630fb16162e3c0b8e2fd810dbf6b7f02e9405e60
srazi94 <sajjadrazi94@gmail.com>
2021-01-10 19:54:02 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, CoreModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
